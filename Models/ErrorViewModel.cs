acd792ae4f7ea153869426ecbbfc4fd27936c915
koss054 <konstantin054@abv.bg>
2022-11-07 00:04:33 +0200

    Add initial project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace techIE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
