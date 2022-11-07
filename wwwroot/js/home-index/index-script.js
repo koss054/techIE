9843d7b8399034b645af7c4e9c312a120dcddbab
koss054 <konstantin054@abv.bg>
2022-11-07 18:02:29 +0200

    Add browse our cateogires static images

--- wwwroot/js/home-index/index-script.js
+++ wwwroot/js/home-index/index-script.js
@@ -1,13 +1,13 @@
 ﻿$(".right-arrow").click(function () {
     $(".right-arrow").css({ "display": "none" });
-    $(".left-arrow").css({ "display": "block"});
+    $(".left-arrow").css({ "display": "flex"});
     $(".hero-first").css({ "display": "none" });
-    $(".hero-second").css({ "display": "block" });
+    $(".hero-second").css({ "display": "flex" });
 });
 
 $(".left-arrow").click(function () {
     $(".left-arrow").css({ "display": "none" });
-    $(".right-arrow").css({ "display": "block" });
+    $(".right-arrow").css({ "display": "flex" });
     $(".hero-second").css({ "display": "none" });
-    $(".hero-first").css({ "display": "block" });
+    $(".hero-first").css({ "display": "flex" });
 });
