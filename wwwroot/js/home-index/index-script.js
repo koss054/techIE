a04fd9b8088a0d046b1c9483e5edec4d614749e7
koss054 <konstantin054@abv.bg>
2022-11-07 16:29:05 +0200

    Make left and right arrow in hero images functional

---
+++ wwwroot/js/home-index/index-script.js
@@ -0,0 +1,13 @@
+﻿$(".right-arrow").click(function () {
+    $(".right-arrow").css({ "display": "none" });
+    $(".left-arrow").css({ "display": "block"});
+    $(".hero-first").css({ "display": "none" });
+    $(".hero-second").css({ "display": "block" });
+});
+
+$(".left-arrow").click(function () {
+    $(".left-arrow").css({ "display": "none" });
+    $(".right-arrow").css({ "display": "block" });
+    $(".hero-second").css({ "display": "none" });
+    $(".hero-first").css({ "display": "block" });
+});
