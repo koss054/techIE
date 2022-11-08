e25cfced049743ca5fe92efa4493fbde27be25ae
koss054 <konstantin054@abv.bg>
2022-11-09 00:41:56 +0200

    Replace default identity user with our expanded user entity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace techIE.Data
+﻿namespace techIE.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using Entities;
+
+    /// <summary>
+    /// Context for our database.
+    /// Inherits the Identity Context with our expanded User entity.
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        /// <summary>
+        /// Overriding the base On Model Creating.
+        /// Adding additional requirements/validations for our database entities.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<MarketplaceProduct> MarketplaceProducts { get; set; }
+
+        public DbSet<OfficialProduct> OfficialProducts { get; set; }
     }
 }
