1d96485605e3ec7e29a6dfbc14673c091507157b
koss054 <konstantin054@abv.bg>
2022-11-08 23:43:05 +0200

    Add color enum, used in the products

---
+++ Data/Entities/Enums/Color.cs
@@ -0,0 +1,18 @@
+﻿namespace techIE.Data.Entities.Enums
+{
+    public enum Color
+    {
+        Colorful = 0,
+        Black = 1,
+        White = 2,
+        Silver = 3,
+        Gold = 4,
+        Blue = 5,
+        Red = 6,
+        Purple = 7,
+        Yellow = 8,
+        Orange = 9,
+        Green = 10,
+        Pink = 11
+    }
+}
