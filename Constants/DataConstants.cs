f0e28ca067c0e03aab99f0746ad5fcc335d35e10
koss054 <konstantin054@abv.bg>
2022-11-08 23:34:06 +0200

    Add file for storing the data constants

---
+++ Constants/DataConstants.cs
@@ -0,0 +1,41 @@
+﻿namespace techIE.Constants
+{
+    /// <summary>
+    /// Class, keeping the constants used for entity validation/requirement.
+    /// </summary>
+    public class DataConstants
+    {
+        /// <summary>
+        /// Constants for the Category entity.
+        /// </summary>
+        public class Category
+        {
+            public const int MinNameLength = 3;
+            public const int MaxNameLength = 30;
+        }
+
+        /// <summary>
+        /// Constants for the Marketplace Product entity.
+        /// </summary>
+        public class MarketplaceProduct
+        {
+
+        }
+
+        /// <summary>
+        /// Constants for the Official Product entity.
+        /// </summary>
+        public class OfficialProduct
+        {
+
+        }
+
+        /// <summary>
+        /// Constants for the extension to the Identity User.
+        /// </summary>
+        public class User
+        {
+
+        }
+    }
+}
