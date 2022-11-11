17cb233790f6ef5ad4aa8075ffaa5f96313e8083
koss054 <konstantin054@abv.bg>
2022-11-11 20:16:12 +0200

    Add order entity to project

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -29,7 +29,8 @@
 
         public DbSet<Category> Categories { get; set; }
 
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ Data/Entities/Order.cs
@@ -0,0 +1,43 @@
+﻿namespace techIE.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Order entity.
+    /// Tracks the orders of the user.
+    /// Keeps the total value of the order, the user who has made the order and the products that were ordered.
+    /// </summary>
+    public class Order
+    {
+        /// <summary>
+        /// Order ID.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Total value of the order.
+        /// </summary>
+        [Required]
+        public decimal TotalValue { get; set; }
+
+        /// <summary>
+        /// ID of the user who has made the order.
+        /// </summary>
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// User who has made the order.
+        /// </summary>
+        [Required]
+        public User User { get; set; } = null!;
+
+        /// <summary>
+        /// A collection of the ordered products.
+        /// The total value is calculated by using this property.
+        /// </summary>
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
