d94c8ff1b27599dcb5273e624f3333d0622006e6
koss054 <konstantin054@abv.bg>
2022-11-12 15:45:05 +0200

    Add script for showing and hiding password

---
+++ wwwroot/js/account/show-pass-script.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var pass = document.getElementById("pass");
+
+    $(".show-pass-div").click(function () {
+        $(".show-pass-div").css({ "display": "none" });
+        $(".hide-pass-div").css({ "display": "flex" });
+        pass.type = "text";
+    });
+
+    $(".hide-pass-div").click(function () {
+        $(".hide-pass-div").css({ "display": "none" });
+        $(".show-pass-div").css({ "display": "flex" });
+        pass.type = "password";
+    });
+})
+
