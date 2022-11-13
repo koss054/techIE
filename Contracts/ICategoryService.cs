5e2e669862dcf831f03f9672cd56fd3a2ff134ad
koss054 <konstantin054@abv.bg>
2022-11-13 21:06:09 +0200

    Update category and user contracts

--- Contracts/ICategoryService.cs
+++ Contracts/ICategoryService.cs
@@ -1,9 +1,22 @@
 ﻿namespace techIE.Contracts
 {
+    using Models.Categories;
+
     /// <summary>
     /// Handling all category logic.
     /// </summary>
     public interface ICategoryService
     {
+        /// <summary>
+        /// Gets all categories from the database.
+        /// </summary>
+        /// <returns>List of categories.</returns>
+        Task<IEnumerable<CategoryViewModel>> GetAllAsync();
+
+        /// <summary>
+        /// Add a category to the database.
+        /// </summary>
+        /// <param name="model"></param>
+        Task AddAsync(AddCategoryViewModel model);
     }
 }
--- Contracts/IUserService.cs
+++ Contracts/IUserService.cs
@@ -1,9 +1,24 @@
 ﻿namespace techIE.Contracts
 {
+    using Models;
+
     /// <summary>
     /// Handling some of the user related logic.
     /// </summary>
     public interface IUserService
     {
+        /// <summary>
+        /// Get current user by using their ID.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>User with provided ID.</returns>
+        Task<UserViewModel?> GetUserAsync(string userId);
+
+        /// <summary>
+        /// Checks if the current user is an admin or not.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Value of the bool user property IsAdmin.</returns>
+        Task<bool> IsAdminAsync(string userId);
     }
 }
