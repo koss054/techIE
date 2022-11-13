791fdabc045f921c664671853690302f451124a9
koss054 <konstantin054@abv.bg>
2022-11-13 21:02:11 +0200

    Add category and user service

---
+++ Services/CategoryService.cs
@@ -0,0 +1,54 @@
+﻿namespace techIE.Services
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Contracts;
+    using Data;
+    using Data.Entities;
+    using Models.Categories;
+
+    /// <summary>
+    /// Handling all category logic.
+    /// </summary>
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext context;
+
+        public CategoryService(AppDbContext _context)
+        {
+            context = _context;
+        }
+
+        /// <summary>
+        /// Gets all categories from the database.
+        /// </summary>
+        /// <returns>List of categories.</returns>
+        public async Task<IEnumerable<CategoryViewModel>> GetAllAsync()
+        {
+            return await context.Categories
+                .Select(c => new CategoryViewModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    IsOfficial = c.IsOfficial
+                }).ToListAsync();
+        }
+
+        /// <summary>
+        /// Add a category to the database.
+        /// </summary>
+        /// <param name="model"></param>
+        public async Task AddAsync(AddCategoryViewModel model)
+        {
+            var entity = new Category()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                IsOfficial = model.IsOfficial
+            };
+
+            await context.Categories.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿namespace techIE.Services
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Contracts;
+    using Models;
+    using Data;
+
+    /// <summary>
+    /// Handling some of the user related logic.
+    /// </summary>
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext context;
+
+        public UserService(AppDbContext _context)
+        {
+            context = _context;
+        }
+
+        /// <summary>
+        /// Get current user by using their ID.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>User with provided ID.</returns>
+        public async Task<UserViewModel?> GetUserAsync(string userId)
+        {
+            return await context.Users
+                .Select(u => new UserViewModel()
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    Email = u.Email,
+                    IsAdmin = u.IsAdmin
+                }).FirstOrDefaultAsync(u => u.Id == userId);
+        }
+
+        /// <summary>
+        /// Checks if the current user is an admin or not.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns>Value of the bool user property IsAdmin.</returns>
+        public async Task<bool> IsAdminAsync(string userId)
+        {
+            var user = await GetUserAsync(userId);
+            return user == null 
+                ? false 
+                : user.IsAdmin;
+        }
+    }
+}
