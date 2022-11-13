a400a19168af2283369975bfd6337713b8774f67
koss054 <konstantin054@abv.bg>
2022-11-13 18:30:49 +0200

    Add initial category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿namespace techIE.Controllers
+{
+    /// <summary>
+    /// Controller for the category entity.
+    /// Only accessible by admin users.
+    /// The allowed users can add more categories to the database.
+    /// </summary>
+    public class CategoryController : BaseController
+    {
+
+    }
+}
