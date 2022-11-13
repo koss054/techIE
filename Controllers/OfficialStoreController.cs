cc23f4ae8581a100783058246488b1a17774d3dd
koss054 <konstantin054@abv.bg>
2022-11-13 17:53:11 +0200

    Add initial official store controller

---
+++ Controllers/OfficialStoreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace techIE.Controllers
+{
+    /// <summary>
+    /// Controller that handles the logic for techIE's official store.
+    /// Only items with IsOfficial == true will be displayed here.
+    /// </summary>
+    public class OfficialStoreController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
