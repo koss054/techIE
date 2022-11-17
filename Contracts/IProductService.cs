2f765cea36e57c576ae8307d286fc8eaadddf370
koss054 <konstantin054@abv.bg>
2022-11-17 22:46:21 +0200

    Update product service to get all official categories and products

--- Contracts/IProductService.cs
+++ Contracts/IProductService.cs
@@ -1,6 +1,7 @@
 ﻿namespace techIE.Contracts
 {
-    using Data.Entities;
+    using Models.Products;
+    using Models.Categories;
 
     /// <summary>
     /// Handling all product logic.
@@ -8,9 +9,16 @@
     public interface IProductService
     {
         /// <summary>
+        /// Get all products with IsOfficial == true.
+        /// Main use is in the admin product panel.
+        /// </summary>
+        /// <returns>List of official products.</returns>
+        Task<IEnumerable<ProductAdminPanelViewModel>> GetAllOfficialAsync();
+
+        /// <summary>
         /// Get all of the official categories from the database.
         /// </summary>
         /// <returns>A list containing all of the official categories that are currently added to the database.</returns>
-        Task<IEnumerable<Category>> GetOfficialCategoriesAsync();
+        Task<IEnumerable<CategoryViewModel>> GetOfficialCategoriesAsync();
     }
 }
--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -3,17 +3,57 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    using Microsoft.EntityFrameworkCore;
+
     using Contracts;
-    using Data.Entities;
+    using Models.Products;
+    using Models.Categories;
+    using Data;
 
     /// <summary>
     /// Handling all product logic.
     /// </summary>
     public class ProductService : IProductService
     {
-        public Task<IEnumerable<Category>> GetOfficialCategoriesAsync()
+        private readonly AppDbContext context;
+
+        public ProductService(AppDbContext _context)
         {
-            throw new NotImplementedException();
+            context = _context;
+        }
+
+        /// <summary>
+        /// Get all products with IsOfficial == true.
+        /// Main use is in the admin product panel.
+        /// </summary>
+        /// <returns>List of official products.</returns>
+        public async Task<IEnumerable<ProductAdminPanelViewModel>> GetAllOfficialAsync()
+        {
+            return await context.Products
+                .Where(p => p.IsOfficial)
+                .Select(p => new ProductAdminPanelViewModel()
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Category = p.Category.Name,
+                    IsOfficial = p.IsOfficial
+                }).ToListAsync();
+        }
+
+        /// <summary>
+        /// Get all of the official categories from the database.
+        /// </summary>
+        /// <returns>A list containing all of the official categories that are currently added to the database.</returns>
+        public async Task<IEnumerable<CategoryViewModel>> GetOfficialCategoriesAsync()
+        {
+            return await context.Categories
+                .Where(c => c.IsOfficial)
+                .Select(c => new CategoryViewModel()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    IsOfficial = c.IsOfficial
+                }).ToListAsync();
         }
     }
 }
