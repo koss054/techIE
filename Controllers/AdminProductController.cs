c927b90ef08d11cedb4325da497e2ba76418bbc9
koss054 <konstantin054@abv.bg>
2022-11-18 02:07:14 +0200

    Add edit get and post, and delete request

--- Controllers/AdminProductController.cs
+++ Controllers/AdminProductController.cs
@@ -77,5 +77,82 @@
                 RedirectPaths.UpdateProductPage,
                 RedirectPaths.UpdateProductController);
         }
+
+        /// <summary>
+        /// Get request for editing a product.
+        /// </summary>
+        /// <param name="id">Id for the product that the admin wants to edit.</param>
+        /// <returns>Post request with model to edit.</returns>
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            // It's no issue that userId may be null.
+            // IsAdminAsync returns false if no users' ID matches the provided one.
+            if (!userService.IsAdminAsync(User.FindFirst(ClaimTypes.NameIdentifier)?.Value))
+            {
+                return RedirectToAction("PLACEHOLDER", "PLACEHOLDER");
+            }
+
+            // **********************************************************************************
+            // TODO: Confirm if it's pointless to convert between database entity and view model.
+            //       ----- If so, validation should be done differently.
+            // **********************************************************************************
+            var entity = await productService.GetAsync(id);
+            var model = new ProductFormViewModel()
+            {
+                Name = entity.Name,
+                Price = entity.Price,
+                ImageUrl = entity.ImageUrl,
+                Color = entity.Color,
+                Description = entity.Description,
+                Categories = await categoryService.GetOfficialAsync(),
+                CategoryId = entity.CategoryId
+            };
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// Post request for editing a product.
+        /// </summary>
+        /// <param name="model">View model with validations.</param>
+        /// <returns>If model is valid, user is redirected to admin category panel. Otherwise, they are prompted to edit the name again.</returns>
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Categories = await categoryService.GetOfficialAsync();
+                return View(model);
+            }
+
+            await productService.EditAsync(model);
+            return RedirectToAction(
+                RedirectPaths.UpdateProductPage,
+                RedirectPaths.UpdateProductController);
+        }
+
+        /// <summary>
+        /// Delete product from database.
+        /// </summary>
+        /// <param name="id">Id of product that will be deleted.</param>
+        /// <returns>Returns to panel page if successful.</returns>
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!userService.IsAdminAsync(User.FindFirst(ClaimTypes.NameIdentifier)?.Value))
+            {
+                return RedirectToAction("PLACEHOLDER", "PLACEHOLDER");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("PLACE", "HOLDER");
+            }
+
+            await productService.DeleteAsync(id);
+            return RedirectToAction(
+                RedirectPaths.UpdateProductPage,
+                RedirectPaths.UpdateProductController);
+        }
     }
 }
