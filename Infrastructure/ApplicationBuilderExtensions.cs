be39d29589505ed56ccd594cefc89a2f5127cbd8
koss054 <konstantin054@abv.bg>
2022-11-24 11:05:43 +0200

    Add summary to ApplicationBuilderExtensions

--- Infrastructure/ApplicationBuilderExtensions.cs
+++ Infrastructure/ApplicationBuilderExtensions.cs
@@ -6,8 +6,14 @@
 
     using static AdminConstants;
 
+    /// <summary>
+    /// Extending the ApplicationBuilder in order to add the role Administrator to the seeded Admin user.
+    /// </summary>
     public static class ApplicationBuilderExtensions
     {
+        /// <summary>
+        /// Used in Program.cs to make the seeded Admin user an Administrator.
+        /// </summary>
         public static IApplicationBuilder SeedAdmin(this IApplicationBuilder app)
         {
             using var scopedServices = app.ApplicationServices.CreateScope();
@@ -16,6 +22,8 @@
             var userManager = services.GetRequiredService<UserManager<User>>();
             var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
+            // If the role already exists, it isn't created again.
+            // If not, it's created and it's assigned to the seeded Admin - we get this user with his email.
             Task.Run(async () =>
             {
                 if (await roleManager.RoleExistsAsync(AdminRoleName))
