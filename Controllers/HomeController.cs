42af2f5901feacb060797e2e413a2db6ef695f48
koss054 <konstantin054@abv.bg>
2022-11-24 18:33:09 +0200

    Update HomeController - summaries, remove unnecessary parts, and AllowAnonymous

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,26 +3,39 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Authorization;
 
+    /// <summary>
+    /// Home controller.
+    /// The index page is here.
+    /// Error pages and additional info about this company is also here.
+    /// </summary>
     public class HomeController : BaseController
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        /// <summary>
+        /// Index page of techIE.
+        /// It has the hero image and access to all categories under it.
+        /// </summary>
         [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Privacy policy.
+        /// Still empty. May abandon completely.
+        /// </summary>
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Error pages.
+        /// Depending on the status code of the error the appropriate error page is displayed.
+        /// </summary>
+        /// <param name="statusCode">Status code of the error.</param>
+        [AllowAnonymous]
         public IActionResult Error(int statusCode)
         {
             if (statusCode == 400 || statusCode == 404)
