13eb1573c01f208a78888719c48c7b253884c007
koss054 <konstantin054@abv.bg>
2022-11-28 23:10:06 +0200

    Add Marketplace area to project

---
+++ Areas/Marketplace/Controllers/StoreController.cs
@@ -0,0 +1,18 @@
+﻿namespace techIE.Areas.Marketplace.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using techIE.Controllers;
+
+    /// <summary>
+    /// Store controller for the user marketplace.
+    /// Allows the users to browse the listings of others and create their own.
+    /// </summary>
+    public class StoreController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
