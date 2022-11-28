4a498696997ed7747f108095d2a81b21d3160bfb
koss054 <konstantin054@abv.bg>
2022-11-29 00:22:00 +0200

    Add initial marketplace StoreController functionality

--- Areas/Marketplace/Controllers/StoreController.cs
+++ Areas/Marketplace/Controllers/StoreController.cs
@@ -2,17 +2,35 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
+    using techIE.Contracts;
     using techIE.Controllers;
 
     /// <summary>
     /// Store controller for the user marketplace.
     /// Allows the users to browse the listings of others and create their own.
     /// </summary>
+    [Area("Marketplace")]
     public class StoreController : BaseController
     {
-        public IActionResult Index()
+        private readonly IProductService productService;
+
+        public StoreController(IProductService _productService)
         {
-            return View();
+            productService = _productService;
+        }
+
+        /// <summary>
+        /// The index page of the user marketplace.
+        /// Gets three random products.
+        /// False is passed to the productService method so only products added by the users are taken.
+        /// Other user marketplace options can also be accessed from here.
+        /// </summary>
+        /// <returns>The index page of the marketplace StoreController.</returns>
+        public async Task<IActionResult> Index()
+        {
+            // Value in async method is false so only user added products are added.
+            var model = await productService.GetThreeRandomAsync(false);
+            return View(model);
         }
     }
 }
