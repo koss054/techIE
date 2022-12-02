6c66cc7b29f4f261ee2349ac5d208695a7f8a0cc
koss054 <konstantin054@abv.bg>
2022-12-02 23:59:58 +0200

    Create ProductSorting enum

---
+++ Data/Entities/Enums/ProductSorting.cs
@@ -0,0 +1,15 @@
+﻿namespace techIE.Data.Entities.Enums
+{
+    /// <summary>
+    /// Used for sorting the products in the user marketplace.
+    /// The first three values are the official techIE categories.
+    /// The last value represents unnofficial categories.
+    /// </summary>
+    public enum ProductSorting
+    {
+        Phones = 0,                     // Official
+        Laptops = 1,                    // Official
+        SmartWatches = 2,               // Official
+        Custom = 3
+    }
+}
