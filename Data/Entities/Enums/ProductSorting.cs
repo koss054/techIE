9f946fdec30102ac0536c2fb30768fb59994d696
koss054 <konstantin054@abv.bg>
2022-12-03 00:17:52 +0200

    Update ProductSorting so it works with search filter

--- Data/Entities/Enums/ProductSorting.cs
+++ Data/Entities/Enums/ProductSorting.cs
@@ -1,15 +1,12 @@
 ﻿namespace techIE.Data.Entities.Enums
 {
     /// <summary>
-    /// Used for sorting the products in the user marketplace.
-    /// The first three values are the official techIE categories.
-    /// The last value represents unnofficial categories.
+    /// Used for sorting the products when a search bar is present on a page.
     /// </summary>
     public enum ProductSorting
     {
-        Phones = 0,                     // Official
-        Laptops = 1,                    // Official
-        SmartWatches = 2,               // Official
-        Custom = 3
+        Newest = 0,                     
+        Price = 1,                    
+        Alphabetical = 2               
     }
 }
