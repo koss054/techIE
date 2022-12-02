580fb3e007accb01de8f529acddbccc9853b8bb8
koss054 <konstantin054@abv.bg>
2022-12-03 01:08:36 +0200

    Add functionality to Explore in marketplace StoreController

--- Areas/Marketplace/Controllers/StoreController.cs
+++ Areas/Marketplace/Controllers/StoreController.cs
@@ -4,6 +4,7 @@
 
     using techIE.Contracts;
     using techIE.Controllers;
+    using techIE.Models.Products;
 
     /// <summary>
     /// Store controller for the user marketplace.
@@ -13,10 +14,14 @@
     public class StoreController : BaseController
     {
         private readonly IProductService productService;
+        private readonly ICategoryService categoryService;
 
-        public StoreController(IProductService _productService)
+        public StoreController(
+            IProductService _productService,
+            ICategoryService _categoryService)
         {
             productService = _productService;
+            categoryService = _categoryService;
         }
 
         /// <summary>
@@ -39,9 +44,20 @@
         /// They can sort the categories from the search bar.
         /// </summary>
         /// <returns>The explore page of the marketplace StoreController.</returns>
-        public async Task<IActionResult> Explore()
+        public async Task<IActionResult> Explore([FromQuery] ProductQueryViewModel query)
         {
-            return View();
+            var productCategories = await categoryService.GetAllNamesAsync();
+            var queryResult = await productService.GetSearchResultAsync(
+                query.Category,
+                query.SearchTerm,
+                query.Sorting,
+                query.CurrentPage,
+                ProductQueryViewModel.ProductsPerPage);
+
+            query.TotalProductsCount = queryResult.TotalProductCount;
+            query.Products = queryResult.Products;
+            query.Categories = productCategories;
+            return View(query);
         }
     }
 }
