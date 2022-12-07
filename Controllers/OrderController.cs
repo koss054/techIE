daa9fa9c897ebcbab5477534002e2f21a87e5621
koss054 <konstantin054@abv.bg>
2022-12-08 01:16:56 +0200

    Create initial Add action in OrderController

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,19 +2,36 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
+    using Constants;
+    using Contracts;
+    using Infrastructure;
+
     /// <summary>
     /// Controller for managing orders.
     /// Used in both official and marketplace area.
     /// </summary>
     public class OrderController : BaseController
     {
+        private readonly IOrderService orderService;
+
+        public OrderController(IOrderService _orderService)
+        {
+            orderService = _orderService;
+        }
+
         /// <summary>
         /// Adding a product to the current order.
         /// </summary>
         /// <param name="id">Id of product that is being ordered.</param>
-        public async Task<IActionResult> Add(int id)
+        /// <param name="area">Name of area from where the action has been called.</param>
+        /// <returns>Redirects the user to appropriate store index page.</returns>
+        public async Task<IActionResult> Add(int id, string area)
         {
-            return View();
+            await orderService.AddAsync(id, this.User.Id());
+            return RedirectToAction(
+                RedirectPaths.AddProductToOrderPage,
+                RedirectPaths.AddProductToOrderController,
+                new { area = "Marketplace" });
         }
     }
 }
