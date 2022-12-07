48446f27d58a38224aa4335139c7619373a3b85e
koss054 <konstantin054@abv.bg>
2022-12-08 00:10:05 +0200

    Create OrderController.cs

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿namespace techIE.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Controller for managing orders.
+    /// Used in both official and marketplace area.
+    /// </summary>
+    public class OrderController : BaseController
+    {
+        /// <summary>
+        /// Adding a product to the current order.
+        /// </summary>
+        /// <param name="id">Id of product that is being ordered.</param>
+        public async Task<IActionResult> Add(int id)
+        {
+            return View();
+        }
+    }
+}
