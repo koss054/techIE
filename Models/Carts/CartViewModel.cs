a0394c4cc67ed58a5aa29db3c72d5626f0ec7552
koss054 <konstantin054@abv.bg>
2022-12-08 23:27:31 +0200

    Create initial view models for cart functionality

---
+++ Models/Carts/CartViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace techIE.Models.Carts
+{
+    using Models.Products;
+
+    public class CartViewModel
+    {
+        /// <summary>
+        /// Id of the cart.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Id of the user who is filling up the cart.
+        /// </summary>
+        public string UserId { get; set; } = null!;
+
+        /// <summary>
+        /// Total of all products that are currently in the cart.
+        /// </summary>
+        public decimal Total { get; set; }
+
+        /// <summary>
+        /// Products that are in the current cart.
+        /// </summary>
+        public IEnumerable<ProductCartFormViewModel> Products { get; set; } = new List<ProductCartFormViewModel>();
+    }
+}
---
+++ Models/Products/ProductCartFormViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace techIE.Models.Products
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductCartFormViewModel
+    {
+        /// <summary>
+        /// Id of product.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of product.
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Price of product.
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Image url of product.
+        /// techIE "owns" imgur and they use it to host the images.
+        /// </summary>
+        public string ImageUrl { get; set; } = null!;
+
+        /// <summary>
+        /// Quantity of the product that is in the cart.
+        /// </summary>
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
