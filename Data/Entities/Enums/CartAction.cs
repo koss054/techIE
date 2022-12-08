49a6c1d37abfb7390da1d17e1cd1a1229de87ff4
koss054 <konstantin054@abv.bg>
2022-12-08 23:26:59 +0200

    Create CartAction.cs

---
+++ Data/Entities/Enums/CartAction.cs
@@ -0,0 +1,12 @@
+﻿namespace techIE.Data.Entities.Enums
+{
+    /// <summary>
+    /// Enums for actions that the cart service returns.
+    /// </summary>
+    public enum CartAction
+    {
+        Successful = 0,
+        Duplicate = 1,
+        Failed = 2
+    }
+}
