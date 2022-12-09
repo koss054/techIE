77b9525e1163cd1b7c2b13a1348638ce648a4703
koss054 <konstantin054@abv.bg>
2022-12-09 23:52:43 +0200

    Add base OrderController functionality

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -18,5 +18,22 @@
         {
             orderService = _orderService;
         }
+
+        /// <summary>
+        /// Checkout the products in the current cart of the user.
+        /// </summary>
+        /// <param name="cartId">The cart that is being finalized.</param>
+        /// <returns>Redirects the user to their order history page.</returns>
+        public async Task<IActionResult> Finish(int cartId)
+        {
+            await orderService.FinishAsync(cartId);
+            return RedirectToAction("Inspect", "Cart");
+        }
+
+        public async Task<IActionResult> History()
+        {
+            var model = await orderService.GetHistoryAsync(this.User.Id());
+            return View(model);
+        }
     }
 }
