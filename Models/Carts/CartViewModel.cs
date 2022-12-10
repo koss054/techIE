252094ad4e33f0f3a6431c453b78e4b1d3a10710
koss054 <konstantin054@abv.bg>
2022-12-10 23:52:57 +0200

    Add IsCurrent to CartViewModel

--- Models/Carts/CartViewModel.cs
+++ Models/Carts/CartViewModel.cs
@@ -20,6 +20,13 @@
         public decimal Total { get; set; }
 
         /// <summary>
+        /// If the user is still filling up the same cart.
+        /// If not, a new cart is created.
+        /// Used in partial view checks when in this view model.
+        /// </summary>
+        public bool IsCurrent { get; set; }
+
+        /// <summary>
         /// Products that are in the current cart.
         /// </summary>
         public IEnumerable<ProductCartViewModel> Products { get; set; } = new List<ProductCartViewModel>();
