d473fdcf99e4c453cf069f92e169db4bb00550be
koss054 <konstantin054@abv.bg>
2022-12-14 23:13:33 +0200

    Update OrderServiceTests.cs

--- techIE.UnitTests/Services/OrderServiceTests.cs
+++ techIE.UnitTests/Services/OrderServiceTests.cs
@@ -8,6 +8,7 @@
 
     using techIE.Contracts;
     using techIE.Data;
+    using techIE.Data.Entities;
     using techIE.Services;
 
     using techIE.UnitTests.Data;
@@ -32,26 +33,34 @@
         [Test]
         public async Task Test_FinishAsync_ValidCartId()
         {
-            var cartId = 1;
+            var cartId = 4;
             var cart = await context.Carts.FirstOrDefaultAsync(c => c.Id == cartId);
+            var expectedTotalValue = await cartService.GetTotalAsync(cartId);
 
             await orderService.FinishAsync(cartId);
-            var order = await context.Orders.FirstOrDefaultAsync();
 
-            Assert.AreNotEqual(order, null);
-            Assert.AreEqual(cart.IsCurrent, false);
-            Assert.AreEqual(await cartService.GetTotalAsync(cartId), order.TotalValue);
+            // Getting the order that was added last.
+            var order = await context.Orders.LastOrDefaultAsync();
+
+            Assert.AreNotEqual(null, order);
+            Assert.AreNotEqual(null, cart);
+            Assert.AreEqual(false, cart.IsCurrent);
+            Assert.AreEqual(expectedTotalValue, order.TotalValue);
         }
 
         [Test]
         public async Task Test_FinishAsync_InvalidCartId()
         {
             var cartId = 999;
+            var expectedOrderCount = 0;
 
+            // If cart is invalid, no order is added to the database.
             await orderService.FinishAsync(cartId);
-            var order = await context.Orders.FirstOrDefaultAsync();
 
-            Assert.AreEqual(order, null);
+            // Expected result is that no order with the provided cartId should be in the database.
+            var order = await context.Orders.Where(o => o.CartId == cartId).ToListAsync();
+
+            Assert.AreEqual(expectedOrderCount, order.Count());
         }
 
         [Test]
@@ -62,7 +71,7 @@
 
             var orders = await orderService.GetHistoryAsync(userId);
 
-            Assert.AreEqual(orders.Count(), expectedOrderCount);
+            Assert.AreEqual(expectedOrderCount, orders.Count());
         }
 
         [Test]
@@ -73,7 +82,7 @@
 
             var orders = await orderService.GetHistoryAsync(userId);
 
-            Assert.AreEqual(orders.Count(), expectedCount);
+            Assert.AreEqual(expectedCount, orders.Count());
         }
     }
 }
