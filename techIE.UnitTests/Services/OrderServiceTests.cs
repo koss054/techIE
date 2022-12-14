869328b5d5669a5e0866bb0f79e5097b05ad8640
koss054 <konstantin054@abv.bg>
2022-12-14 21:54:40 +0200

    Update OrderServiceTests.cs

--- techIE.UnitTests/Services/OrderServiceTests.cs
+++ techIE.UnitTests/Services/OrderServiceTests.cs
@@ -1,63 +1,79 @@
 ﻿namespace techIE.UnitTests.Services
 {
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading;
     using System.Threading.Tasks;
 
     using Microsoft.EntityFrameworkCore;
-    using Moq;
     using NUnit.Framework;
 
     using techIE.Contracts;
     using techIE.Data;
-    using techIE.Data.Entities;
     using techIE.Services;
 
-    public class MockedDb
-    {
-
-    }
+    using techIE.UnitTests.Data;
 
     [TestFixture]
     public class OrderServiceTests
     {
-        private AppDbContext contextMock;
-        private CartService cartService;
-        private OrderService orderService;
+        private AppDbContext context;
+        private ICartService cartService;
+        private IOrderService orderService;
 
-        //[SetUp]
-        //public void SetUpDbContext()
-        //{
-        //    contextMock.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1);
-        //}
-
-        public OrderServiceTests()
+        [SetUp]
+        public void TestInitialize()
         {
-            var options = new DbContextOptionsBuilder<AppDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
+            var testContext = new TestDbContext();
+            context = testContext.GetSeededEntities();
 
-            contextMock = new AppDbContext(options);
-            cartService = new CartService(contextMock);
-            orderService = new OrderService(contextMock, cartService);
+            cartService = new CartService(context);
+            orderService = new OrderService(context, cartService);
         }
 
         [Test]
-        public async Task Test_FinishAsync_Valid()
+        public async Task Test_FinishAsync_ValidCartId()
         {
-            var cart = new Cart()
-            {
-                Id = 1,
-                IsCurrent = true,
-            };
+            var cartId = 1;
+            var cart = await context.Carts.FirstOrDefaultAsync(c => c.Id == cartId);
 
-            contextMock.Carts.Add(cart);
-            await orderService.FinishAsync(cart.Id);
+            await orderService.FinishAsync(cartId);
+            var order = await context.Orders.FirstOrDefaultAsync();
 
+            Assert.AreNotEqual(order, null);
             Assert.AreEqual(cart.IsCurrent, false);
+            Assert.AreEqual(await cartService.GetTotalAsync(cartId), order.TotalValue);
+        }
+
+        [Test]
+        public async Task Test_FinishAsync_InvalidCartId()
+        {
+            var cartId = 999;
+
+            await orderService.FinishAsync(cartId);
+            var order = await context.Orders.FirstOrDefaultAsync();
+
+            Assert.AreEqual(order, null);
+        }
+
+        [Test]
+        public async Task Test_GetHistoryAsync_ValidUser()
+        {
+            var expectedOrderCount = 2;
+            var userId = "a9ad02b6-f60f-4bae-b99a-83fbacbb0c9b";
+
+            var orders = await orderService.GetHistoryAsync(userId);
+
+            Assert.AreEqual(orders.Count(), expectedOrderCount);
+        }
+
+        [Test]
+        public async Task Test_GetHistoryAsync_InvalidUser()
+        {
+            var expectedCount = 0;
+            var userId = "invalid-user-id-guid";
+
+            var orders = await orderService.GetHistoryAsync(userId);
+
+            Assert.AreEqual(orders.Count(), expectedCount);
         }
     }
 }
