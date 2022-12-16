9b6855794dee43322112d0d47cd49d6ed8c14fb9
koss054 <konstantin054@abv.bg>
2022-12-16 14:46:07 +0200

    Create Messages.cs

---
+++ techIE/Constants/Messages.cs
@@ -0,0 +1,9 @@
+﻿namespace techIE.Constants
+{
+    public static class Messages
+    {
+        // Cart Controller View Data
+        public const string CartActionSuccessful = "Product successfully added to your cart!";
+        public const string CartActionDuplicate = "Product quantity successfully increased!";
+    }
+}
