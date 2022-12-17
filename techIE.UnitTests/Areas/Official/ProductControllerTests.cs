9c08680025302c799c32b662de11b933f700e6d7
koss054 <konstantin054@abv.bg>
2022-12-17 17:25:08 +0200

    Create official ProductControllerTests

---
+++ techIE.UnitTests/Areas/Official/ProductControllerTests.cs
@@ -0,0 +1,124 @@
+﻿namespace techIE.UnitTests.Areas.Official
+{
+    using System.Collections.Generic;
+
+    using Moq;
+    using NUnit.Framework;
+    using Microsoft.AspNetCore.Mvc;
+
+    using techIE.Contracts;
+    using techIE.Areas.Official.Controllers;
+    using techIE.Models.Categories;
+    using techIE.Models;
+    using techIE.Models.Products;
+
+    public class ProductControllerTests
+    {
+        private ProductController controller;
+        private Mock<IUserService> userServiceMock;
+        private Mock<IProductService> productServiceMock;
+
+        [SetUp]
+        public void Tests_Initialize()
+        {
+            userServiceMock = new Mock<IUserService>();
+            productServiceMock = new Mock<IProductService>();
+
+            controller = new ProductController(
+                userServiceMock.Object,
+                productServiceMock.Object);
+        }
+
+        [Test]
+        public void Test_DetailsValid_ReturnsView()
+        {
+            // Arrange
+            userServiceMock
+                .Setup(u => u.GetUserByProductIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new UserViewModel());
+
+            productServiceMock
+                .Setup(p => p.GetDetailedAsync(It.IsAny<int>(), It.IsAny<UserViewModel>()))
+                .ReturnsAsync(new ProductDetailedViewModel() { IsOfficial = true });
+
+            // Act
+            var action = controller.Details(It.IsAny<int>());
+
+            // Assert
+            Assert.That(action.Result, Is.TypeOf<ViewResult>());
+        }
+
+        [Test]
+        public void Test_DetailsNoSeller_ReturnsBadRequest()
+        {
+            // Arrange
+            UserViewModel seller = null;
+            userServiceMock
+                .Setup(u => u.GetUserByProductIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(seller);
+            // Act
+            var action = controller.Details(It.IsAny<int>());
+
+            // Assert
+            Assert.That(action.Result, Is.TypeOf<BadRequestResult>());
+        }
+
+        [Test]
+        public void Test_DetailsNoModel_ReturnsNotFound()
+        {
+            // Arrange
+            ProductDetailedViewModel model = null;
+            userServiceMock
+                .Setup(u => u.GetUserByProductIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new UserViewModel());
+
+            productServiceMock
+                .Setup(p => p.GetDetailedAsync(It.IsAny<int>(), It.IsAny<UserViewModel>()))
+                .ReturnsAsync(model);
+
+            // Act
+            var action = controller.Details(It.IsAny<int>());
+
+            // Assert
+            Assert.That(action.Result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void Test_DetailsDeletedModel_ReturnsNotFound()
+        {
+            // Arrange
+            userServiceMock
+                .Setup(u => u.GetUserByProductIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new UserViewModel());
+
+            productServiceMock
+                .Setup(p => p.GetDetailedAsync(It.IsAny<int>(), It.IsAny<UserViewModel>()))
+                .ReturnsAsync(new ProductDetailedViewModel() { IsDeleted = true });
+
+            // Act
+            var action = controller.Details(It.IsAny<int>());
+
+            // Assert
+            Assert.That(action.Result, Is.TypeOf<NotFoundResult>());
+        }
+
+        [Test]
+        public void Test_DetailsMarketplaceProduct_ReturmsRedirectToAction()
+        {
+            // Arrange
+            userServiceMock
+                .Setup(u => u.GetUserByProductIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(new UserViewModel());
+
+            productServiceMock
+                .Setup(p => p.GetDetailedAsync(It.IsAny<int>(), It.IsAny<UserViewModel>()))
+                .ReturnsAsync(new ProductDetailedViewModel() { IsOfficial = false });
+
+            // Act
+            var action = controller.Details(It.IsAny<int>());
+
+            // Assert
+            Assert.That(action.Result, Is.TypeOf<RedirectToActionResult>());
+        }
+    }
+}
